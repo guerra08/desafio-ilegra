624ef8fe64aa50f13a90cb91efa292f1c1528db1
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2020-10-10 00:26:34 -0300

    Feat: @guerra08
    Documents and diagrams.

--- src/main/java/service/Service.java
+++ src/main/java/service/Service.java
@@ -1,6 +1,10 @@
 package service;
 
 public abstract class Service {
+
     public abstract boolean addFromProcessedData(String[] data);
+
     public abstract void cleanRepository();
+
+    public abstract String generateOutputString();
 }
