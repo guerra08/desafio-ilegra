d1454e4778bf8814040dad623a650a063970ca24
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2020-10-11 14:04:06 -0300

    Feat: @guerra08
    Public modifier.

--- src/test/java/service/SalesmanServiceTest.java
+++ src/test/java/service/SalesmanServiceTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class SalesmanServiceTest {
+class SalesmanServiceTest {
 
     @Test
     void testAddSaleFromDataString(){
