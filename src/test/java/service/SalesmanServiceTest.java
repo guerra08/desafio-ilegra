0bced9868015033202a08c4f0bab5d9cb7a971e4
Bruno Guerra <bruno.guerra@edu.pucrs.br>
2020-10-11 14:01:37 -0300

    Feat: @guerra08
    Test service.

---
+++ src/test/java/service/SalesmanServiceTest.java
@@ -0,0 +1,33 @@
+package service;
+
+import config.Characters;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SalesmanServiceTest {
+
+    @Test
+    void testAddSaleFromDataString(){
+        SalesmanService salesmanService = new SalesmanService();
+        String[] split = "003ç10ç[1-10-100,2-30-2.50,3-40-3.10]çDiego".split(Characters.MAIN_SEPARATOR);
+        assertFalse(salesmanService.addFromProcessedData(split));
+    }
+
+    @Test
+    void testAddSalesmanFromDataString(){
+        SalesmanService salesmanService = new SalesmanService();
+        String[] split = "001ç3245678865434çRenatoç40000.99".split(Characters.MAIN_SEPARATOR);
+        assertTrue(salesmanService.addFromProcessedData(split));
+    }
+
+    @Test
+    void testAddInvalidFromDataString(){
+        SalesmanService salesmanService = new SalesmanService();
+        String[] split = "21ç1234567891234çDiegoç".split(Characters.MAIN_SEPARATOR);
+        assertFalse(salesmanService.addFromProcessedData(split));
+    }
+
+
+}
